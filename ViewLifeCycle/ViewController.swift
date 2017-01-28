27bb3409cd4ed297f1b2f7713cfcff3e355a45c7
bgb0923 <bgb0923@gmail.com>
2017-01-28 17:29:47 -0500

    Followed along with lesson

--- ViewLifeCycle/ViewController.swift
+++ ViewLifeCycle/ViewController.swift
@@ -13,12 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad() called")
-        view.backgroundColor = UIColor.randomColor()
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("viewWillAppear(_:) called")
+        view.backgroundColor = UIColor.randomColor()
     }
     
     override func viewDidAppear(_ animated: Bool) {
